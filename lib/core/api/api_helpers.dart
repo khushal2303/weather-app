047575f4bbbe799ef5df092961fd2f9a85a553e7
KV-Dahlia <khushal.vadher@dahlia.tech>
2025-03-26 11:29:29 +0530

    fix: remove api key.

--- lib/core/api/api_helpers.dart
+++ lib/core/api/api_helpers.dart
@@ -1,6 +1,6 @@
 class ApiHelpers {
   static String baseUrl = "https://api.openweathermap.org/data/2.5/";
-  static String apiKey = "34bc940bb63fcab732259797a3975715";
+  static String apiKey = "YOUR_API_KEY";
 
   static const String weather = "weather";
   static const String forecast = "forecast";
